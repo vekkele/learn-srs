0f4f01c042c8e357000ca62cbf729d562fef77ba
Vlad Keliy <vekkele@gmail.com>
2023-01-19 21:23:33 +0530

    🔥 Remove example trpc router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
 import { type NextPage } from "next";
 import { signIn, useSession } from "next-auth/react";
-import Button from "../components/Button";
-
-import { api } from "../utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const { data: sessionData } = useSession();
 
   return (
@@ -14,14 +10,12 @@ const Home: NextPage = () => {
 
       <div className="flex flex-col items-center gap-2">
         <p>{sessionData?.user?.name}</p>
-        <p className="text-2xl text-white">
-          {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-        </p>
-        <Button
+        <button
+          className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
           onClick={() => void signIn(undefined, { callbackUrl: '/dashboard' })}
         >
           Sign in
-        </Button>
+        </button>
       </div>
     </main>
   );
