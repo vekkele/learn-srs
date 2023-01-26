90a3c3d05d7a6ed04968c65f2742e323bc956855
Vlad Keliy <vekkele@gmail.com>
2023-01-26 17:48:15 +0530

    ➕ Add clsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,14 @@
+import clsx from "clsx";
 import type { ButtonHTMLAttributes } from "react";
 
 const Button = ({ onClick, disabled, children, className = "", ...props }: ButtonHTMLAttributes<HTMLButtonElement>) => {
-  const disabledStyles = 'opacity-40 cursor-not-allowed';
-  const regularStyles = 'hover:bg-blue-800';
-
   return (
     <button
-      className={`${className} ${disabled ? disabledStyles : regularStyles} bg-blue-600 rounded-lg  px-10 py-3 font-semibold text-white no-underline transition `}
+      className={clsx(
+        className,
+        disabled ? 'opacity-40 cursor-not-allowed' : 'hover:bg-blue-800',
+        'bg-blue-600 rounded-lg px-10 py-3 font-semibold text-white no-underline transition',
+      )}
       onClick={onClick}
       disabled={disabled}
       {...props}
