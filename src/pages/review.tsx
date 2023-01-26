08ef6580e7a21fe001874da63058f4101b6408b9
Vlad Keliy <vekkele@gmail.com>
2023-01-26 10:44:38 +0530

    💄 Add styles for review page

--- src/pages/review.tsx
+++ src/pages/review.tsx
@@ -5,6 +5,7 @@ import type {
 } from "next";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import Button from "../components/Button";
 import { prisma } from "../server/db";
 import ReviewQueue from "../types/ReviewQueue";
 import { api } from "../utils/api";
@@ -12,12 +13,24 @@ import { checkAuthedSession } from "../utils/auth";
 
 type ReviewPageProps = InferGetServerSidePropsType<typeof getServerSideProps>;
 
+const getInputColor = (correct: boolean | null) => {
+  if (correct === null) {
+    return 'bg-white';
+  }
+
+  return correct ? 'bg-green-600' : 'bg-red-600'
+}
+
 const ReviewPage: NextPage<ReviewPageProps> = ({ words }) => {
   const [queue, setQueue] = useState(() => ReviewQueue.from(words))
   const [correct, setCorrect] = useState<null | boolean>(null);
   const [guess, setGuess] = useState("");
+  const [infoVisible, setInfoVisible] = useState(false);
   const router = useRouter();
   const mutation = api.learn.updateStage.useMutation();
+  const answered = correct !== null;
+  const word = queue.next;
+  const toggleInfoVisible = () => setInfoVisible(visible => !visible);
 
   useEffect(() => {
     if (queue.isEmpty) {
@@ -49,18 +62,41 @@ const ReviewPage: NextPage<ReviewPageProps> = ({ words }) => {
 
     setQueue(v => new ReviewQueue(correct ? v.handleCorrect() : v.handleIncorrect()))
     setCorrect(null);
+    setGuess("");
   }
 
-
   return (
-    <main>
-      <div>Try to guess: {queue.next?.word}</div>
-      <div>{queue.next?.translations[0]?.translation}</div>
-      <div>Incorrect: {queue.next?.incorrectAnswers}</div>
+    <main className="flex flex-col h-full grow">
+      <section className="flex justify-center items-center h-[50vh] w-full bg-purple-600">
+        <h1 className="text-7xl text-white uppercase">{word?.word}</h1>
+      </section>
+      <section className="flex items-start grow">
+        <div className="flex flex-col items-center w-full">
+          <div className="relative w-3/4 -translate-y-1/2">
+            <input
+              type="text"
+              name="guess"
+              readOnly={answered}
+              className={`w-full px-2 py-4 text-xl rounded-xl text-center ${getInputColor(correct)}`}
+              value={guess}
+              placeholder="Enter one of translations"
+              onChange={(e) => setGuess(e.target.value)}
+            />
 
-      <input type="text" name="guess" onChange={(e) => setGuess(e.target.value)} />
-      {correct !== null && <div>{correct ? "correct" : "incorrect"}</div>}
-      <button className="block" onClick={check}>next</button>
+            <button className="absolute w-12 h-12 rounded-full top-0 bottom-0 ml-2 my-auto bg-slate-600 text-white" onClick={check}>{'>>'}</button>
+          </div>
+
+          <Button disabled={!answered} onClick={toggleInfoVisible}>Show Info</Button>
+          {infoVisible && (
+            <section className="bg-slate-400 py-4 px-6 my-3 rounded-lg">
+              <h2 className="text-xl font-bold mb-2">Translations</h2>
+              {word?.translations.map(t => (
+                <h2 key={t.translation}>{t.translation}</h2>
+              ))}
+            </section>
+          )}
+        </div>
+      </section>
     </main>
   );
 }
