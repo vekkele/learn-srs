ff182ec2a28162d633e0ce5d0d6b08631ffa11be
Vlad Keliy <vekkele@gmail.com>
2023-01-26 10:44:14 +0530

    💄 Add disabled styles for Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,14 @@
 import type { ButtonHTMLAttributes } from "react";
 
-const Button = ({ onClick, children, className = "", ...props }: ButtonHTMLAttributes<HTMLButtonElement>) => {
+const Button = ({ onClick, disabled, children, className = "", ...props }: ButtonHTMLAttributes<HTMLButtonElement>) => {
+  const disabledStyles = 'opacity-40 cursor-not-allowed';
+  const regularStyles = 'hover:bg-blue-800';
+
   return (
     <button
-      className={`${className} rounded-lg bg-blue-600 px-10 py-3 font-semibold text-white no-underline transition hover:bg-blue-800`}
+      className={`${className} ${disabled ? disabledStyles : regularStyles} bg-blue-600 rounded-lg  px-10 py-3 font-semibold text-white no-underline transition `}
       onClick={onClick}
+      disabled={disabled}
       {...props}
     >
       {children}
