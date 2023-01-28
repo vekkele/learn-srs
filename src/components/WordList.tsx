d77460bb952bc3f9d087124562682189454ab9b3
Vlad Keliy <vekkele@gmail.com>
2023-01-28 17:04:39 +0530

    ✨ Add filtering by stage in word list

--- src/components/WordList.tsx
+++ src/components/WordList.tsx
@@ -1,26 +1,74 @@
+import clsx from "clsx";
+import { useMemo, useState } from "react";
 import { api } from "../utils/api";
+import type { StageTitle } from "../utils/stage";
+import { getStageFromLevel } from "../utils/stage";
+import { stages } from "../utils/stage";
 import WordCard from "./WordCard";
 
 const WordList = () => {
-  const wordsResponse = api.learn.getWords.useQuery();
+  const wordsResponse = api.learn.getWords.useQuery(undefined, {
+    select: (words) => words.map((w) => ({
+      ...w, stage: {
+        ...w.stage,
+        title: getStageFromLevel(w.stage.level)
+      }
+    }))
+  });
+  const [selectedStage, setSelectedStage] = useState<StageTitle | null>(null);
+
+  const stagesCount = useMemo(() => {
+    const words = wordsResponse.data;
+
+    return stages.map((stage) => {
+      return {
+        ...stage,
+        count: words?.reduce(
+          (prev, w) =>
+            w.stage.title === stage.title
+              ? prev + 1
+              : prev,
+          0
+        ) ?? 0
+      }
+    });
+  }, [wordsResponse.data]);
+
+  const selectedStageWords = useMemo(() =>
+    wordsResponse.data?.filter((word) => word.stage.title === selectedStage),
+    [selectedStage, wordsResponse.data]
+  );
 
   if (wordsResponse.isLoading) {
     return <div>Word List Loading...</div>
   }
 
-  const words = wordsResponse.data;
-  if (!words) {
-    return <div>Something went wrong fetching words</div>
-  }
-
   return (
     <section className="flex flex-col items-center my-5">
       <h2 className="mb-2">Word List</h2>
-      <div className="flex flex-wrap gap-2">
-        {words.map((word) => (
-          <WordCard key={word.id} word={word} />
+      <div className="grid grid-flow-col auto-cols-fr gap-4 flex-wrap">
+        {stagesCount.map(stage => (
+          <div
+            key={stage.title}
+            onClick={() => setSelectedStage(prev => prev === stage.title ? null : stage.title)}
+            className={clsx(
+              stage.bg,
+              'flex flex-col items-center py-4 px-10 rounded-lg text-white select-none',
+              { 'cursor-pointer': stage.count }
+            )}
+          >
+            <h3 className="font-bold text-2xl mb-1">{stage.count}</h3>
+            <h5 className="text-md text-neutral-300 capitalize">{stage.title}</h5>
+          </div>
         ))}
       </div>
+      {selectedStage && (
+        <section className="w-full mt-3">
+          {selectedStageWords?.map((word) => (
+            <WordCard key={word.id} word={word} />
+          ))}
+        </section>
+      )}
     </section>
   );
 }
--- src/utils/stage.ts
+++ src/utils/stage.ts
@@ -1,3 +1,5 @@
+import clsx from "clsx";
+
 export const computeNextLearn = (hoursToNextStage: number) => {
   const nextLearn = new Date(Date.now() + hoursToNextStage * 60 * 60 * 1000);
   nextLearn.setMinutes(0, 0, 0);
@@ -13,3 +15,24 @@ export const getNextStage = (currentStage: number, incorrectAnswers: number) => 
 
   return Math.max(1, currentStage - incorrectAdjustmentCount * srsPenaltyFactor);
 }
+
+export function getStageFromLevel(level: number) {
+  const stage = stages.find(stage => level >= stage.minLevel && level <= stage.maxLevel)?.title;
+  if (!stage) {
+    console.error(`no stage exists for level ${level}`);
+    return stages[0].title;
+  }
+
+  return stage;
+}
+
+export const stages = [
+  { title: 'apprentice', minLevel: 1, maxLevel: 4, bg: clsx('bg-pink-600') },
+  { title: 'guru', minLevel: 5, maxLevel: 6, bg: clsx('bg-purple-600') },
+  { title: 'master', minLevel: 7, maxLevel: 7, bg: clsx('bg-blue-600') },
+  { title: 'enlightened', minLevel: 8, maxLevel: 8, bg: clsx('bg-sky-600') },
+  { title: 'burned', minLevel: 9, maxLevel: 9, bg: clsx('bg-stone-600') },
+] as const
+
+export type StageData = (typeof stages)[number];
+export type StageTitle = StageData['title'];
