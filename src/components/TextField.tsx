94684ff5b72760376705d9ab17fbd97e9807197c
Vlad Keliy <vekkele@gmail.com>
2023-01-31 10:21:59 +0530

    💄 Remove TextField theme colors and make it full width

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -7,9 +7,9 @@ interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
 
 const TextField = ({ label, className, ...props }: TextFieldProps) => {
   return (
-    <label className="flex flex-col">
+    <label className="flex flex-col w-full">
       {label && (
-        <span className="text-gray-900 dark:text-white font-bold mb-2">
+        <span className="font-bold mb-2">
           {label}
         </span>
       )}
