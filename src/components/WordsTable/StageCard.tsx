4d51ad4be0edf422984eeb7d1653ced1933e7a29
Vlad Keliy <vekkele@gmail.com>
2023-02-08 21:33:05 +0530

    ♿️ Make stage title more contrast

--- src/components/WordsTable/StageCard.tsx
+++ src/components/WordsTable/StageCard.tsx
@@ -23,9 +23,7 @@ const StageCard = ({ title, color, wordsCount, onClick }: StageCardProps) => {
       )}
     >
       <h3 className="mb-1 text-2xl font-bold">{wordsCount}</h3>
-      <h5 className="text-md capitalize text-neutral-300">
-        {t(`stages.${title}`)}
-      </h5>
+      <h5 className="text-md capitalize">{t(`stages.${title}`)}</h5>
     </button>
   );
 };
