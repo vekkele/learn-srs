2a4260efc1fbdb81691a4df12dc9769911ff3472
Vlad Keliy <vekkele@gmail.com>
2023-02-11 11:50:13 +0530

    ✨ Add review forecast basic visuals

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -3,6 +3,7 @@ import type { GetServerSideProps, NextPage } from "next";
 import { useTranslation } from "next-i18next";
 import Link from "next/link";
 import AuthHeader from "../components/AuthHeader";
+import ReviewForecast from "../components/ReviewForecast";
 import WordsTable from "../components/WordsTable";
 import { api } from "../utils/api";
 import { checkAuthedSession } from "../utils/auth";
@@ -12,32 +13,39 @@ const buttonClass = clsx(
   "py-5 px-4 bg-blue-600 hover:bg-blue-800 rounded-xl transition text-2xl text-white capitalize"
 );
 
+const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
 const Dashboard: NextPage = () => {
   const { t } = useTranslation("dashboard");
   const { data: reviewsCount } = api.learn.getReviewsCount.useQuery();
+  const { data: forecast } = api.learn.getReviewForecast.useQuery({ timezone });
 
   return (
     <>
       <AuthHeader />
       <main className="flex flex-col items-center p-10">
-        <div className="grid auto-cols-fr grid-flow-col gap-5">
-          <Link
-            href="/card/add"
-            className={clsx(buttonClass, "flex flex-col items-center")}
-          >
-            {t("addWord")}
-          </Link>
+        <section className="flex w-full items-start justify-center">
+          <section className="mr-auto grid auto-cols-fr grid-flow-col gap-5">
+            <Link
+              href="/card/add"
+              className={clsx(buttonClass, "flex flex-col items-center")}
+            >
+              {t("addWord")}
+            </Link>
 
-          <Link
-            href="/review"
-            className={clsx(buttonClass, "flex items-center justify-between")}
-          >
-            <h2>{t("reviews")}</h2>
-            <h3 className="ml-4 rounded-full bg-blue-400 px-3 text-sm">
-              {reviewsCount}
-            </h3>
-          </Link>
-        </div>
+            <Link
+              href="/review"
+              className={clsx(buttonClass, "flex items-center justify-between")}
+            >
+              <h2>{t("reviews")}</h2>
+              <h3 className="ml-4 rounded-full bg-blue-400 px-3 text-sm">
+                {reviewsCount}
+              </h3>
+            </Link>
+          </section>
+
+          {forecast && <ReviewForecast forecast={forecast} />}
+        </section>
 
         <WordsTable />
       </main>
